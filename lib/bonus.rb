0f0db2363675e0c833230b07fc30a6af350c65ac
LaNice Powell <lanicemp@aol.com>
2019-07-03 17:41:48 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -3,7 +3,7 @@ def bonus
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive", "dead"},
+      :hero => {name: "Romeo", age: "15", status: ["alive", "dead"]},
       status[:second]
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
